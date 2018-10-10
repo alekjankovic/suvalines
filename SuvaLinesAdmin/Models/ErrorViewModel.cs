d32082605cc1ed572173beac8ca11aac43ede777
alekjankovic <ajankovic983@gmail.com>
2018-10-10 17:59:21 +0200

    Admin Project Added

---
+++ SuvaLinesAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuvaLinesAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
