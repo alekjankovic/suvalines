ec294718f66d55ca6bd621e58500069af3aa3c31
alekjankovic <ajankovic983@gmail.com>
2018-10-25 14:13:45 +0200

    Vue App Added

---
+++ SuvaLinesSPA/wwwroot/App.vue
@@ -0,0 +1,27 @@
+﻿<template>
+    <div>
+        <app-header></app-header>
+    </div>
+
+</template>
+
+<script>
+
+    import Header from './components/Header.vue';
+
+
+    export default {
+        components: {
+            'app-header': Header
+        }
+        data() {
+            return {
+            }
+        }
+
+    }
+
+</script>
+
+<style>
+</style>
---
+++ SuvaLinesSPA/wwwroot/main.js
@@ -0,0 +1,6 @@
+﻿import App from '.App.vue';
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+});
