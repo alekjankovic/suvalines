f8743d49197377e7f9552168d15c5f1e3e718482
alekjankovic <ajankovic983@gmail.com>
2019-01-18 15:40:02 +0100

    New project added

---
+++ SuvaLinesVue/Models/EditModels/SuvaEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuvaLinesVue.Models.EditModels
+{
+    public class SuvaEditModel
+    {
+        private readonly SuvaLinesContext _context;
+
+        SuvaEditModel(SuvaLinesContext context) {
+            _context = context;       
+        }
+
+    }
+}
